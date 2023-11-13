2a403a4f9466b240110a9de50ad8afea7c29b222
Tobia Ochsner <tobia.ochsner@hotmail.com>
2023-11-13 20:21:27 +0100

    Add translation invariance test.

--- tests/test_invariances.py
+++ tests/test_invariances.py
@@ -52,3 +52,53 @@ def test_rotation_invariance():
 
     assert np.allclose(solver.q, np.array([v.position for v in vertices]))
     assert np.allclose(solver.v, np.array([v.velocity for v in vertices]))
+
+
+def test_rotation_translation_invariance():
+    """Test if the solver is invariant w.r.t rotation and translation of the
+    mesh."""
+    vertices = [
+        Vertex(
+            position=np.array([1.0, 0.0, 0.0]),
+            velocity=np.array([0.0, 0.0, 0.0]),
+            mass=1.0,
+            external_force=np.array([0.0, 0.0, 0.0]),
+        ),
+        Vertex(
+            position=np.array([3.0, 0.0, 0.0]),
+            velocity=np.array([0.0, 0.0, 0.0]),
+            mass=1.0,
+            external_force=np.array([0.0, 0.0, 0.0]),
+        ),
+        Vertex(
+            position=np.array([1.0, 3.0, 3.0]),
+            velocity=np.array([0.0, 0.0, 0.0]),
+            mass=1.0,
+            external_force=np.array([0.0, 0.0, 0.0]),
+        ),
+    ]
+
+    triangles = [Triangle(0, 1, 2)]
+
+    rotation = Rotation.from_euler("xy", [30, 50], degrees=True)
+
+    constraints: list[PDConstraint] = [
+        Simplicial2DConstraint(
+            triangle=triangles[0],
+            initial_positions=rotation.apply(
+                np.array([[1.0, 0.0, 0.0], [3.0, 0.0, 0.0], [1.0, 3.0, 3.0]])
+                + np.array([[10, 20, -10]])
+            ),
+            sigma_min=-1.0,
+            sigma_max=1.0,
+            weight=1,
+        )
+    ]
+
+    solver = ProjectiveDynamicsSolver(vertices, constraints)
+
+    for _ in range(100):
+        solver.perform_step(100)
+
+    assert np.allclose(solver.q, np.array([v.position for v in vertices]))
+    assert np.allclose(solver.v, np.array([v.velocity for v in vertices]))
