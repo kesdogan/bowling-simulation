e3da1ce74172d1b72fdb53ab0091ab1f0216f205
tlk13 <timurkesdogan@gmail.com>
2023-11-25 13:27:18 +0100

    changed objects into one mesh

--- src/run.py
+++ src/run.py
@@ -1,57 +1,27 @@
-import igl
-import k3d
-import numpy as np
-import openmesh as om
+import k3d, igl
+import numpy as np 
 import polyscope as ps
 
 
-class Object:
-    def __init__(self, v, f, velocities=None, forces=None):
-        self.v = v
-        self.f = f
-        if not velocities:
-            self.velocities = np.zeros_like(v)
-        else:
-            self.velocities = velocities
-        if not forces:
-            self.forces = np.zeros_like(v)
-        else:
-            self.forces = forces
 
-    def update(self, dt):
-        self.v += self.velocities * dt
-        self.velocities += self.forces * dt
-        self.forces = np.zeros_like(self.v)
-
-
-dt = 0.1
-
-
-# load pins from obj file and generate them in a bowling setting
-v, _, _, f, _, _ = igl.read_obj(
-    "/Users/timurkesdogan/Desktop/simulation/project/bowling-simulation/assets/simple_pin.obj"
-)
-pins = []
-for i in range(2):
-    for j in range(-i, i + 1):
-        pins.append(
-            Object(v + np.array([i * 1.5, 0, 0]) + np.array([0, 0, j * 1.5]), f)
-        )
-
-
-# load bowling ball from obj file place in field
-v, _, _, f, _, _ = igl.read_obj(
-    "/Users/timurkesdogan/Desktop/simulation/project/bowling-simulation/assets/simple_ball.obj"
-)
+# load bowling ball from obj file place in field 
+v, _, _, f, _, _ = igl.read_obj("../assets/simple_ball.obj")
 v = v + np.array([-10, 0, 0])
 
+# create global faces and vertices np.arrays 
+vertices = np.array(v)
+faces = np.array(f)
 
+# add pins to the same mesh in this array 
+v, _, _, f, _, _ = igl.read_obj("../assets/simple_pin.obj")
+for i in range(2): 
+    for j in range(-i, i+1): 
+        faces = np.concatenate((faces, f + len(vertices)), axis=0)
+        vertices = np.concatenate((vertices, (v+np.array([i*1.5, 0 ,0])+ np.array([0,0, j*1.5]))), axis=0)
+
+
+
+# register complete mesh w/ polyscope
 ps.init()
-
-# register the pins
-for counter, pin in enumerate(pins):
-    ps.register_surface_mesh("pin" + str(counter), pin.v, pin.f)
-
-# register the ball
-ps.register_surface_mesh("ball", v, f)
+ps.register_surface_mesh("everything", vertices, faces)
 ps.show()
